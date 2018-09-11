f4d0003859b86888d1de707331b67f97c8b120ca
Guilherme Ferreira Silva <42393633+wilhelmfsilva@users.noreply.github.com>
2018-09-11 00:27:14 -0300

    P1.c

---
+++ P1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+  int estado = 0, i=0;
+  char bits[256];
+    
+  scanf("%s", bits);
+  
+  while(bits[i] != '\0'){
+    if(estado == 0 && bits[i] == '0'){
+    estado = 0;
+    }
+    else if(estado == 0 && bits[i] == '1'){
+    estado = 1;
+    }
+    else if(estado == 1 && bits[i] == '1'){
+    estado = 0;
+    }
+    else if(estado == 1 && bits[i] == '0'){
+    estado = 1;
+    }
+    i++;
+  }
+       
+  printf("\n\"%s\" ", bits);
+       
+if(estado == 0) {
+  printf("e multiplo de 3\n");
+}
+else if(estado == 1) {
+  printf("nao e\n");
+}
+  return 0;
+}
