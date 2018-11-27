5ec35e0de203bb772eee53bba8cd41332079828a
Guilherme Ferreira Silva <42393633+wilhelmfsilva@users.noreply.github.com>
2018-11-27 17:10:06 -0200

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int A(int, int);
+
+int main(){
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+    
+    n=A(m, n);
+
+    printf("%d\n", n);
+    
+    return 0;
+}
+
+int A(int m ,int n){
+    
+    if(m==0){
+        n++;
+}
+    else if(m>0 && n==0){
+        n=A(m-1,1);
+}
+    else if(m>0 && n>0){
+        n=A(m-1,A(m,n-1));
+}
+
+    return n;
+}
